7cafaaca064c5d804255ffa434a320720a905aa1
monalisazyx <madhurimazyx@gmail.com>
2023-10-23 11:22:44 +0530

    Add function to operate on numbers based on operator

--- clac.js
+++ clac.js
@@ -12,3 +12,27 @@ function multiply(a ,b ){
 function divide(a ,b ){
     return a / b;
 }
+
+let operator;
+let firstNum;
+let secondNum;
+
+function operate(a , b , operator){
+
+    switch(operator){
+        case '+':
+            return add(a , b);
+        case '-':
+            return subtract(a , b);
+        case '*':
+            return multiply(a , b);
+        case '/':
+            return divide(a , b);
+        default:
+            return 'operator not found'
+    }
+            
+  
+
+    
+}
