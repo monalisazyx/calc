38513defb63d4aa761e4baf6aeed5ad77fabfbc8
monalisazyx <madhurimazyx@gmail.com>
2023-10-23 12:33:51 +0530

    Display numbers and operators on webpage

--- clac.js
+++ clac.js
@@ -17,6 +17,18 @@ let operator;
 let firstNum;
 let secondNum;
 
+let display = document.querySelector('.display');
+let btn = document.querySelector('#btnNum');
+for(let i = 0 ; i < 1 ; i++){
+
+ btn.addEventListener('click', (e) => {
+    let x = document.createElement('p');
+    x.textContent = e.target.id;
+    display.appendChild(x);
+   });
+}
+ 
+
 function operate(a , b , operator){
 
     switch(operator){
